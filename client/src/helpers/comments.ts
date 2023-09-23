d4d0e01f14fab002da518684298131dcd66ebda8
Jared McAteer <jared.mcateer@gmail.com>
2023-09-23 00:37:26 -0600

    feat(helpers): add additional comment helpers

--- client/src/helpers/comments.ts
+++ client/src/helpers/comments.ts
@@ -1,5 +1,24 @@
 import ts from "typescript";
 
+export function copySyntheticComments<T extends ts.Node>(node: T, copyNode: ts.Node): T {
+  const fullText = copyNode.getSourceFile().getFullText();
+  const leadingComments = getLeadingComments(fullText, copyNode.pos);
+  const trailingComments = getTrailingComments(fullText, copyNode.end);
+
+  let result = node;
+  for (const { pos, end, kind, hasTrailingNewLine } of leadingComments) {
+    const text = getCommentText(fullText, { pos, end });
+    result = ts.addSyntheticLeadingComment(result, kind, text, hasTrailingNewLine);
+  }
+
+  for (const { pos, end, kind, hasTrailingNewLine } of trailingComments) {
+    const text = getCommentText(fullText, { pos, end });
+    result = ts.addSyntheticTrailingComment(result, kind, text, hasTrailingNewLine);
+  }
+
+  return node;
+}
+
 export function addTodoComment(node: ts.Node, comment: string, multiline: boolean) {
   const kind = multiline
     ? ts.SyntaxKind.MultiLineCommentTrivia
@@ -20,6 +39,19 @@ export function prependSyntheticComments<T extends ts.Node>(node: T, copyNode: t
   });
 
   return node;
+}
+
+export function removeComments<T extends ts.Node>(node: T): T | ts.StringLiteral {
+  if (!ts.isStringLiteral(node)) return node;
+  return ts.factory.createStringLiteral(node.text);
+}
+
+function getLeadingComments(text: string, pos: number): ts.CommentRange[] {
+  return getCommentRange(text, pos, "Leading");
+}
+
+function getTrailingComments(text: string, pos: number): ts.CommentRange[] {
+  return getCommentRange(text, pos, "Trailing");
 }
 
 function getCommentRange(text: string, pos: number, type: "Leading" | "Trailing") {
