5680e99831fefd31e136e775f8601feffa57bace
Jared McAteer <jared.mcateer@gmail.com>
2023-10-14 17:50:37 -0600

    test(transforms): :white_check_mark: add test for @Component decorator emits option

---
+++ client/src/transformer/transforms/vue-class-component/decorator-options/__tests__/Emits.test.ts
@@ -0,0 +1,40 @@
+import { convertAst } from "@/convert.js";
+import { getSingleFileProgram } from "@/parser.js";
+import { describe, expect, it } from "vitest";
+
+describe("Emits", () => {
+  it("should transform emits", () => {
+    const { ast, program } = getSingleFileProgram(`
+      import { Component } from 'vue-class-component';
+      @Component({ emits: ['a', 'b', 'c'] })
+      export default class Foo { }
+    `);
+    const result = convertAst(ast, program);
+
+    expect(result).toMatchInlineSnapshot(`
+      "const emit = defineEmits<{
+          \\"a\\": [
+              ...args: unknown[]
+          ];
+          \\"b\\": [
+              ...args: unknown[]
+          ];
+          \\"c\\": [
+              ...args: unknown[]
+          ];
+      }>();
+      "
+    `);
+  });
+
+  it("should throw an error if the emits option is not an array", () => {
+    const { ast, program } = getSingleFileProgram(`
+      import { Component } from 'vue-class-component';
+      @Component({ emits: 'a' })
+      export default class Foo { }
+    `);
+    expect(() => convertAst(ast, program)).toThrowError(
+      `Invalid Option-Data: emits option should be string[]`,
+    );
+  });
+});
