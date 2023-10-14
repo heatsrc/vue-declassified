5c8cac48c9f213f3c9294c928ac0136affbca831
Jared McAteer <jared.mcateer@gmail.com>
2023-10-14 17:48:27 -0600

    test(file): :white_check_mark: fixing file test from earlier refactor

--- client/src/__tests__/file.test.ts
+++ client/src/__tests__/file.test.ts
@@ -1,26 +1,14 @@
-import { writeFile } from "node:fs/promises";
+import { readFile } from "node:fs/promises";
 import { resolve } from "node:path";
-import { fileURLToPath } from "node:url";
 import { describe, expect, it, vi } from "vitest";
 import { readVueFile, writeVueFile } from "../file.js";
 
-const dirname = fileURLToPath(new URL(".", import.meta.url));
-
-vi.mock("node:fs/promises", async () => {
-  const actual = await vi.importActual<typeof import("node:fs/promises")>("node:fs/promises");
-  return {
-    ...actual,
-    writeFile: vi.fn(),
-  };
-});
-
-const mockWriteFile = vi.mocked(writeFile);
-
 describe("test readFile", () => {
   it("test should read a valid vue file", async () => {
-    const path = resolve(dirname, "./fixtures/TestInput.vue");
+    const path = resolve(__dirname, "./fixtures/TestInput.vue");
+    const content = await readFile(path, { encoding: "utf8" });
 
-    let { script, vueFile } = await readVueFile(path);
+    let { script, vueFile } = await readVueFile(content);
 
     expect(vueFile.descriptor.script).toBeDefined();
     expect(script).toEqual(vueFile.descriptor.script);
@@ -31,26 +19,29 @@ describe("test readFile", () => {
     expect(vueFile.descriptor.styles.length).toBe(1);
   });
 
-  it("should throw an error if the file has no script", () => {
-    const path = resolve(dirname, "./fixtures/MissingScript.vue");
-    expect(() => readVueFile(path)).rejects.toThrowError("Vue file has no script!");
+  it("should throw an error if the file has no script", async () => {
+    const path = resolve(__dirname, "./fixtures/MissingScript.vue");
+    const content = await readFile(path, { encoding: "utf8" });
+    expect(() => readVueFile(content)).rejects.toThrowError("Vue file has no script!");
   });
 
-  it("should throw an error if the file has script setup", () => {
-    const path = resolve(dirname, "./fixtures/ScriptSetup.vue");
-    expect(() => readVueFile(path)).rejects.toThrowError("Vue file already has script setup!");
+  it("should throw an error if the file has script setup", async () => {
+    const path = resolve(__dirname, "./fixtures/ScriptSetup.vue");
+    const content = await readFile(path, { encoding: "utf8" });
+    expect(() => readVueFile(content)).rejects.toThrowError("Vue file already has script setup!");
   });
 });
 
 describe("test writeFile", () => {
-  it("should write a valid vue file", async () => {
-    const { script, vueFile } = await readVueFile(resolve(dirname, "./fixtures/MinInput.vue"));
+  it("should return a valid vue file", async () => {
+    const path = resolve(__dirname, "./fixtures/MinInput.vue");
+    const content = await readFile(path, { encoding: "utf8" });
+    const { script, vueFile } = await readVueFile(content);
     const scriptContent = '"placeholder";';
 
-    await writeVueFile("output.vue", vueFile, scriptContent);
+    const result = await writeVueFile(vueFile, scriptContent);
 
-    expect(mockWriteFile).toHaveBeenCalledWith(
-      "output.vue",
+    expect(result).toBe(
       `<script setup lang="ts">\n` +
         `${scriptContent}\n` +
         `</script>\n\n` +
@@ -62,7 +53,6 @@ describe("test writeFile", () => {
         `  color: red;\n` +
         `}\n` +
         `</style>`,
-      { encoding: "utf8" },
     );
   });
 });
