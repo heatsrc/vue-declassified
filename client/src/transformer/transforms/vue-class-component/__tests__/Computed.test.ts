21c0c0849469339467355fdf385078da1b6b1877
Jared McAteer <jared.mcateer@gmail.com>
2023-11-10 15:25:41 -0700

    test: :white_check_mark: fix lint error

--- client/src/transformer/transforms/vue-class-component/__tests__/Computed.test.ts
+++ client/src/transformer/transforms/vue-class-component/__tests__/Computed.test.ts
@@ -6,6 +6,7 @@ import { describe, expect, it } from "vitest";
 import { mergeComputed, transformGetter, transformSetter } from "../Computed.js";
 
 describe("Computed", () => {
+  const classNode = ts.factory.createClassDeclaration(undefined, "foo", undefined, undefined, []);
   it("should transform getter", () => {
     const { ast, program } = getSingleFileProgram("class foo { get a() { return 1 } }");
     const property = (ast.statements[0] as ts.ClassDeclaration)
@@ -57,7 +58,7 @@ describe("Computed", () => {
       expect(output.shouldContinue).toBe(false);
       const result = output.result;
       if (Array.isArray(result)) throw new Error("Expected result to be a single node");
-      const merged = mergeComputed([result], program);
+      const merged = mergeComputed([result], program, classNode);
       shouldBeTruthy(merged);
       expect(merged[0].tag).toBe("Computed");
       expect(merged[0].reference).toBe(VxReferenceKind.VARIABLE_VALUE);
@@ -84,7 +85,7 @@ describe("Computed", () => {
       const output = [outputGetter.result, outputSetter.result].filter(
         (o): o is VxTransformResult<ts.Node> => !!o,
       );
-      const merged = mergeComputed(output, program);
+      const merged = mergeComputed(output, program, classNode);
       expect(merged.length).toBe(1);
       expect(merged[0].tag).toBe("Computed");
       expect(merged[0].reference).toBe(VxReferenceKind.VARIABLE_VALUE);
@@ -105,7 +106,7 @@ describe("Computed", () => {
       const outputSetter = transformSetter(setter, program);
       expect(outputSetter.result).toBeTruthy();
       const output = [outputSetter.result].filter((o): o is VxTransformResult<ts.Node> => !!o);
-      const merged = mergeComputed(output, program);
+      const merged = mergeComputed(output, program, classNode);
       expect(merged.length).toBe(1);
       expect(merged[0].tag).toBe("Computed");
       expect(merged[0].reference).toBe(VxReferenceKind.VARIABLE_VALUE);
